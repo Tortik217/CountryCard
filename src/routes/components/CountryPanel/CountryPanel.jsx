24d0f6da446216930a87f71d79ff576f32230d02
Иван Алексеевич Коледов <ivan.koledov@chipdip.ru>
2025-05-19 09:58:26 +0300

    push cityes

--- src/routes/components/CountryPanel/CountryPanel.jsx
+++ src/routes/components/CountryPanel/CountryPanel.jsx
@@ -3,24 +3,19 @@ import Country from "../Country/Country";
 import Cities from "../Cities/Cities";
 
 function CountryPanel() {
-
-const films = Cities()
+  const films = Cities();
 
   return (
     <div
-      className="
-    wrapper
-    border border-primary rounded-3
-    p-4
-    "
+      className="wrapper border border-primary rounded-3 p-4"
     >
       <div className="country">
         <Country />
       </div>
       <div className="Cities">
-         {films.map(item => (
-        <div key={item.id}>{item.title}</div>
-      ))}
+        {films.map((item) => (
+          <div key={item.id}>{item.title}</div>
+        ))}
       </div>
     </div>
   );
